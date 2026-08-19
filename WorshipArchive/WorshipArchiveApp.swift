4ff004bd87950f85691296610b67787aa8e99c52
Jiseok Song <jesong1004@naver.com>
2026-08-19 14:34:51 +0900

    chore: initialize WorshipArchive project

---
+++ WorshipArchive/WorshipArchiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorshipArchiveApp.swift
+//  WorshipArchive
+//
+//  Created by JISEOK SONG on 8/19/26.
+//
+
+import SwiftUI
+
+@main
+struct WorshipArchiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
